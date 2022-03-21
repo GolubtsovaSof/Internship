f90a545d0059a3484920e4ef6b0a8c02e8318532
GolubtsovaSof <92886622+GolubtsovaSof@users.noreply.github.com>
2022-03-21 20:14:24 +0300

    preloader

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ function myFunction(id){
     document.getElementById(id).classList.toggle("show");
 }
 
+window.onload = function () {
+    document.body.classList.add('loaded');
+}
+
 ymaps.ready(function () {
     var map;
     ymaps.geolocation.get().then(function (res) {
@@ -17,6 +21,41 @@ ymaps.ready(function () {
     });
     function createMap (state) {
         map = new ymaps.Map('map', state);
+        var layer = map.layers.get(0).get(0);
+        waitForTilesLoad(layer).then(function() {
+        document.body.classList.remove('loaded_hiding');
+        });
     }
 });
 
+function getTileContainer(layer) {
+    for (var k in layer) {
+        if (layer.hasOwnProperty(k)) {
+            if (
+                layer[k] instanceof ymaps.layer.tileContainer.CanvasContainer
+                || layer[k] instanceof ymaps.layer.tileContainer.DomContainer
+            ) {
+                return layer[k];
+            }
+        }
+    }
+    return null;
+}
+
+function waitForTilesLoad(layer) {
+    return new ymaps.vow.Promise(function (resolve, reject) {
+        var tc = getTileContainer(layer), readyAll = true;
+        tc.tiles.each(function (tile, number) {
+            if (!tile.isReady()) {
+                readyAll = false;
+            }
+        });
+        if (readyAll) {
+            resolve();
+        } else {
+            tc.events.once("ready", function() {
+                resolve();
+            });
+        }
+    });
+}
