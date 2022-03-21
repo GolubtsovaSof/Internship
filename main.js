2d9e5e3779ff401136e30b92ac18ee2b4b41aabe
GolubtsovaSof <92886622+GolubtsovaSof@users.noreply.github.com>
2022-03-21 20:09:28 +0300

    map

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 function myFunction(id){
     document.getElementById(id).classList.toggle("show");
 }
+
+ymaps.ready(function () {
+    var map;
+    ymaps.geolocation.get().then(function (res) {
+        var mapContainer = $('#map'),
+            bounds = res.geoObjects.get(0).properties.get('boundedBy'),
+            mapState = ymaps.util.bounds.getCenterAndZoom(
+                bounds,
+                [mapContainer.width(), mapContainer.height()]
+            );
+        createMap(mapState);
+        res.geoObjects.options.set('preset', 'islands#redCircleIcon');
+        map.geoObjects.add(res.geoObjects);
+    });
+    function createMap (state) {
+        map = new ymaps.Map('map', state);
+    }
+});
+
